d782aec84d1be7747c23df0c1c1a4feaef1ebad8
Romain Calascibetta <romain.calascibetta@gmail.com>
2021-04-21 13:18:51 +0200

    First commit

---
+++ lib/prettym.mli
@@ -0,0 +1,94 @@
+type ppf
+type 'a t = ppf -> 'a -> ppf
+
+type ('ty, 'v) order
+
+val keval_order : (ppf -> 'v) -> ppf -> ('ty, 'v) order -> 'ty
+
+val break : indent:int -> len:int -> ('v, 'v) order
+val fws : ('v, 'v) order
+val spaces : int -> ('v, 'v) order
+val cut : ('v, 'v) order
+
+val const : 'a t -> 'a -> ('v, 'v) order
+val atom : 'a t -> ('a -> 'v, 'v) order
+val a : ('a t -> 'a -> 'v, 'v) order
+
+val (!!) : 'a t -> ('a -> 'v, 'v) order
+val ($) : 'a t -> 'a -> ('v, 'v) order
+
+val new_line : ('v, 'v) order
+
+val tbox : int -> ('v, 'v) order
+val bbox : ('v, 'v) order
+val box : ('v, 'v) order
+val close : ('v, 'v) order
+
+val using : ('b -> 'a) -> 'a t -> 'b t
+
+val string : string t
+val bytes : Bytes.t t
+val bigstring : Bigstringaf.t t
+val breakable : string t
+val char : char t
+
+val list : sep:('x t * 'x) -> 'v t -> 'v list t
+val option : 'a t -> 'a option t
+
+type ('ty, 'v) fmt =
+  | [] : ('v, 'v) fmt
+  | (::) : ('x, 'v) order * ('v, 'r) fmt -> ('x, 'r) fmt
+
+val concat : ('a, 'b) fmt -> ('b, 'c) fmt -> ('a, 'c) fmt
+val keval : (ppf -> 'v) -> ppf -> ('ty, 'v) fmt -> 'ty
+val eval : ppf -> ('ty, ppf) fmt -> 'ty
+
+module Buffer : sig
+  type t =
+    | Bigstring of Bigstringaf.t
+    | String of string
+    | Bytes of bytes
+
+  val weight : t -> int
+  (** Weight of {!t}. *)
+
+  val sub : t -> int -> int -> t
+  (** [sub t off len] does a sub operation of {!t} of [len] bytes starting at
+     [off]. *)
+end
+
+module IOVec : sig
+  type t = {buffer: Buffer.t; off: int; len: int}
+  (** Type of IOVec. *)
+
+  val weight : t -> int
+  (** Weight of {!t}. *)
+
+  val length : t -> int
+  (** Length (in bytes) of {!t}. *)
+
+  val lengthv : t list -> int
+  (** Length (in bytes) of a list of {!t}. *)
+
+  val shift : t -> int -> t
+  (** [shift t n] shifts [n] bytes on [t]. *)
+
+  val split : t -> int -> (t * t)
+  (** [split t off] splits [t] at [off] point. *)
+
+  val merge : t -> t -> t option
+  (** [merge a b] tries to merge [a] and [b] into a new {!t}. *)
+end
+
+val io_buffer_size : int
+
+val create : ?margin:int -> ?new_line:string -> emitter:(IOVec.t list -> int) -> int -> ppf
+
+val is_empty : ppf -> bool
+
+val flush : ppf -> ppf
+
+val kflush : (ppf -> 'v) -> ppf -> 'v
+
+val to_string : ?margin:int -> ?new_line:string -> 'a t -> 'a -> string
+val to_stream : ?margin:int -> ?new_line:string -> 'a t -> 'a -> (unit -> string option)
